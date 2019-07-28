d86237d3f39751370a666676b279428e8637f220
Zhenjie Xu <zhenjie.xu@shopee.com>
2019-07-28 17:18:45 +0800

    feat: init

---
+++ wepy.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+var prod = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  wpyExt: '.wpy',
+  eslint: true,
+  cliLogs: !prod,
+  build: {
+  },
+  resolve: {
+    alias: {
+      counter: path.join(__dirname, 'src/components/counter'),
+      '@store': path.join(__dirname, 'src/store'),
+      '@common': path.join(__dirname, 'src/common'),
+      '@mixins': path.join(__dirname, 'src/mixins'),
+      '@': path.join(__dirname, 'src'),
+    },
+    aliasFields: ['wepy', 'weapp'],
+    modules: ['node_modules']
+  },
+  compilers: {
+    less: {
+      compress: prod
+    },
+    babel: {
+      sourceMap: true,
+      presets: [
+        '@babel/preset-env'
+      ],
+      plugins: [
+        '@wepy/babel-plugin-import-regenerator'
+      ]
+    }
+  },
+  plugins: [],
+  appConfig: {
+    noPromiseAPI: ['createSelectorQuery']
+  }
+}
+
